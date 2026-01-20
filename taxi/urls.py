03863614894a51efaa821aee7dde8e831249ecdf
Mykola Nakonechnuy <teddykolia@ukr.net>
2026-01-20 14:10:56 +0200

    'Solution'

--- taxi/urls.py
+++ taxi/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("manufacturers/", ManufacturerListView.as_view(),
          name="manufacturer-list"),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cark/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("drivers/<int:pk>/", DriverDetailView.as_view(),
          name="driver-detail"),
